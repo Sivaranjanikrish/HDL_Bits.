5dd29d1f61338408458d20cb2cea7554308a1b2c
Sivaranjanikrish <shivaranjanikrish4@gmail.com>
2025-12-07 13:40:39 +0530

    Create 12.structural_7458.v

---
+++ Basics_of _verilog/12.structural_7458.v
@@ -0,0 +1,16 @@
+//The 7458 is a chip with four AND gates and two OR gates. Create a module with the same functionality as the 7458 chip. It has 10 inputs and 2 outputs. You may choose to use an assign statement to drive each of the output wires, or you may choose to declare (four) wires for use as intermediate signals, where each internal wire is driven by the output of one of the AND gates.
+//expected module declaration:module top_module ( input p1a, p1b, p1c, p1d, p1e, p1f,output p1y,input p2a, p2b, p2c, p2d,output p2y );
+module top_module ( 
+    input p1a, p1b, p1c, p1d, p1e, p1f,
+    output p1y,
+    input p2a, p2b, p2c, p2d,
+    output p2y );
+    wire and1,and2,and3,and4;
+    and(and1,p2a,p2b);
+    and(and2,p2c,p2d);
+    and(and3,p1a,p1c,p1b);
+    and(and4,p1f,p1e,p1d);
+    or(p2y,and1,and2);
+    or(p1y,and3,and4);
+        endmodule
+	
